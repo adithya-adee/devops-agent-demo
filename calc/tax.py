7322c2d248ea8cd52ec09558352b61c7d34dd527
glitchy_moon <adithya25905@gmail.com>
2026-09-01 02:59:03 +0530

    update tax handling (02:59:02)

--- calc/tax.py
+++ calc/tax.py
@@ -5,4 +5,4 @@ TAX_RATE = 0.10
 
 def apply_tax(amount):
     """Return `amount` with sales tax added."""
-    return amount + amount * TAX_RATE
+    return amount + TAX_RATE  # demo 02:59:02: change requested
